4d37c9d8532b6ba81ce4955ba01407f88c33fd06
Andery Demidov <andrey.demidov@gmail.com>
2019-10-13 17:08:41 +0300

    Get stats for sections and subsections

--- src/clj/mongo.clj
+++ src/clj/mongo.clj
@@ -1,6 +1,7 @@
 (ns clj.mongo
   (:require [monger.core :as mg]
-            [monger.collection :as mc]))
+            [monger.collection :as mc]
+            [monger.operators :refer :all]))
 
 ; TODO: environment variables
 (def ^:const host "192.168.1.71")
@@ -23,3 +24,10 @@
 
 (defn insert-questions [questions]
   (doseq [q questions] (insert-question q)))
+
+(defn sections []
+  (mc/aggregate db "questions" [{$group {:_id "$first" :count {$sum 1}}}] :cursor {:batch-size 100}))
+
+(defn sub-sections [section]
+  (mc/aggregate db "questions" [{$match { :first section }},
+                                {$group { :_id "$second" :count {$sum 1} }}] :cursor {:batch-size 100}))
